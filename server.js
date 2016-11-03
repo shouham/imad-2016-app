b916a947a8a4ad8ae5c7eab517dcae7d13a21a92
shouham <shouham_choudhury@srmuniv.edu.in>
2016-11-03 11:41:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,15 +27,15 @@ app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
 
-$(document).ready(function(){
-	/* This code is executed after the DOM has been completely loaded */
+// $(document).ready(function(){
+// 	/* This code is executed after the DOM has been completely loaded */
 
-	$('nav a,footer a.up').click(function(e){
+// 	$('nav a,footer a.up').click(function(e){
 
-		// If a link has been clicked, scroll the page to the link's hash target:
+// 		// If a link has been clicked, scroll the page to the link's hash target:
 
-		$.scrollTo( this.hash || 0, 1500);
-		e.preventDefault();
-	});
+// 		$.scrollTo( this.hash || 0, 1500);
+// 		e.preventDefault();
+// 	});
 
-});
+// });
