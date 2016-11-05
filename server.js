80304b19f686d3519d44708696a56f6df8543477
shouham <shouham_choudhury@srmuniv.edu.in>
2016-11-05 20:51:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,6 +30,24 @@ app.get('/test-db',function(req,res){
     }); 
 });
 
+app.get('/articles/:articleName',function(req,res){
+    
+    var articleName=req.params.articleName;
+    pool.query("SELECT * FROM article WHERE title =' "+req.params.articleName +"'",function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else {
+            if (result.rows.length === 0){
+                res.status(404).send('Article not found');
+            } else{
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    
+});
+
 app.get('/ui/resume.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'resume.html'));
 });
