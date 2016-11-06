ed0b4a54d61054a951af710fb678591ab90d7380
shouham <shouham_choudhury@srmuniv.edu.in>
2016-11-06 16:53:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ function createTemplate(data){
 app.get('/articles/:articleName',function(req,res){
     
     // var articleName=req.params.articleName;
-    pool.query("SELECT * FROM article WHERE title ='" + req.params.articleName + "'",function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else {
