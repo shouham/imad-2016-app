6906890dfc2454929553c60e61df33a1d7057b1f
shouham <shouham_choudhury@srmuniv.edu.in>
2016-11-07 12:39:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/test-db',function(req,res){
 function createTemplate(titles,dates,headings,contents){
     var title=titles;
     var date=dates;
-    var heading=headings;
+    var heading=heading.headings;
     var content=contents;
     
     var htmlTemplate = '<html><head><title>title</title><meta name="viewport" content="width=device-width,initial-scale=1"/>        <link href="/ui/style.css" rel="stylesheet"/></head><body><div class="container"><div><a href="/">Home</a></div><hr/><h3>{{heading}}</h3><div>${date}</div><div>${content}</div></div><script src="angular/angular.min.js"></script></body></html>';
