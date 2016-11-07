1dde24d55c868ffc67ee91b024e99019fb73ff30
shouham <shouham_choudhury@srmuniv.edu.in>
2016-11-07 14:24:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,15 +40,16 @@ app.get('/articles',function(req,res){
         }else {
             if (result.rows.length === 0){
                 res.status(404).send('Article not found');
-            } else{/**
+            } else{
+                
                 var app = angular.module('shouham',[]);app.controller('SHcontroller',function(){
                 var data = {title: result.rows[0].title, date: result.rows[0].date, content: result.rows[0].content, heading: result.rows[0].heading}; 
                 this.data = data;});
                  var htmlTemplate =  '<html ng-app = "shouham"><head><title>{{title}}</title><meta name="viewport" content="width=device-width,initial-scale=1"/><link href="/ui/style.css" rel="stylesheet"/></head><body><div class="container" ng-controller="SHcontroller as C"><div><a href="/">Home</a></div><hr/><div ng-repeat ="data in C.data"><h3>{{data.heading}}</h3><div>{data.date}</div><div>{{data.content}</div></div></div><script src="angular/angular.min.js"></script></body></html>';
                  
-                res.send(htmlTemplate);*/
-                var data = {title: result.rows[0].title, date: result.rows[0].date, content: result.rows[0].content, heading: result.rows[0].heading}; 
-                res.send(data);
+                res.send(htmlTemplate);
+               /** var data = {title: result.rows[0].title, date: result.rows[0].date, content: result.rows[0].content, heading: result.rows[0].heading}; 
+                res.send(data);*/
             }
         }
     });
