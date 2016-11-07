3f28f513236f5f41d6caeaee87dd369fc3feb816
shouham <shouham_choudhury@srmuniv.edu.in>
2016-11-07 14:48:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/test-db',function(req,res){
 });
 var createTemplate = function (data){
     var heading = data.heading;
-    var htmlTemplate =  '<html><head><title>${datas.title}</title><meta name="viewport" content="width=device-width,initial-scale=1"/><link href="/ui/style.css" rel="stylesheet"/></head><body><div class="container"><div><a href="/">Home</a></div><hr/><h3>${heading}</h3><div>${date}</div><div>${content}</div></div></div><script src="angular/angular.min.js"></script></body></html>';
+    var htmlTemplate =  '<html><head><title>${title}</title><meta name="viewport" content="width=device-width,initial-scale=1"/><link href="/ui/style.css" rel="stylesheet"/></head><body><div class="container"><div><a href="/">Home</a></div><hr/><h3>${heading}</h3><div>${date}</div><div>${content}</div></div></div><script src="angular/angular.min.js"></script></body></html>';
     return htmlTemplate;
 }
 
