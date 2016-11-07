5047a4cf3dfcf665c74d823f3ac68ce7e5438990
shouham <shouham_choudhury@srmuniv.edu.in>
2016-11-07 12:35:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ function createTemplate(titles,dates,headings,contents){
     var heading=headings;
     var content=contents;
     
-    var htmlTemplate = '<html><head><title>${title}</title><meta name="viewport" content="width=device-width,initial-scale=1"/>        <link href="/ui/style.css" rel="stylesheet"/></head><body><div class="container"><div><a href="/">Home</a></div><hr/><h3>{{heading}}</h3><div>${date}</div><div>${content}</div></div></body></html>';
+    var htmlTemplate = '<html><head><title>title</title><meta name="viewport" content="width=device-width,initial-scale=1"/>        <link href="/ui/style.css" rel="stylesheet"/></head><body><div class="container"><div><a href="/">Home</a></div><hr/><h3>{{heading}}</h3><div>${date}</div><div>${content}</div></div></body></html>';
     return htmlTemplate;
 }
 
