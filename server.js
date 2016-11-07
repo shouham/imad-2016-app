a8a022683d108a6fffb6c31aabfb0a6f96aa799d
shouham <shouham_choudhury@srmuniv.edu.in>
2016-11-07 11:52:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,8 @@ app.get('/articles',function(req,res){
                 var date = result.rows[0].date;
                 var heading = result.rows[0].heading;
                 var content = result.rows[0].content;
-                res.send(createTemplate(title,date,heading,content));
+                res.send(JSON.stringify(result.rows[0]));
+                /**res.send(createTemplate(title,date,heading,content));*/
             }
         }
     });
