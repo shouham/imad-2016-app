1313b0a6cde48fbc7bf9b49bd2c328d9b6baa67d
shouham <shouham_choudhury@srmuniv.edu.in>
2016-11-07 12:50:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,12 +31,30 @@ app.get('/test-db',function(req,res){
 });
 
 function createTemplate(titles,dates,headings,contents){
-    var title=titles;
-    var date=dates;
-    var heading=headings;
-    var content=contents;
+    var app = angular.module('shouham',[]).controller('controller',function(){
     
-    var htmlTemplate = '<html><head><title>title</title><meta name="viewport" content="width=device-width,initial-scale=1"/>        <link href="/ui/style.css" rel="stylesheet"/></head><body><div class="container"><div><a href="/">Home</a></div><hr/><h3>{{headings}}</h3><div>${date}</div><div>${content}</div></div><script src="angular/angular.min.js"></script></body></html>';
+    var htmlTemplate = 
+    '<html ng-app = "shouham">
+    <head>
+    <title>
+    {{title}}
+    </title>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>        
+    <link href="/ui/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+    <div class="container" ng-controller="controller as C">
+    <div><a href="/">Home</a></div><hr/>
+    <div ng-repeat ="data in C.data"
+    <h3>{{data.headings}}</h3>
+    <div>{data.date}</div>
+    <div>{{data.content}</div>
+    </div>
+    <script src="angular/angular.min.js"></script>
+    <script>
+    var app = angular.module('shouham',[]).controller('controller',function(){
+    </body>
+    </html>';
     return htmlTemplate;
 }
 
@@ -51,11 +69,13 @@ app.get('/articles',function(req,res){
             if (result.rows.length === 0){
                 res.status(404).send('Article not found');
             } else{
-                var title =result.rows[0].title;
+            var data = {title: result.rows[0].title, date: result.rows[0].date, content: result.rows[0].content, heading: result.rows[0].heading;}; 
+                /**var title =result.rows[0].title;
                 var date = result.rows[0].date;
                 var heading = result.rows[0].heading;
                 var content = result.rows[0].content;
-                res.send(createTemplate(title,date,heading,content));
+                res.send(createTemplate(title,date,heading,content));*/
+                res.send(JSON.stringify.(data));
             }
         }
     });
